647deb8200cb63d1da5232ccaac0404226f5bada
surajbathina-atlan <suraj.bathina@atlan.com>
2021-09-17 16:58:58 +0530

    Adding a debug line

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ mvn -pl '!plugin-kylin,!ranger-kylin-plugin-shim' -DskipJSTests -DskipTests=true
 
 echo "[DEBUG] listing distro/target"
 ls distro/target
+
+echo "[DEBUG] listing /home/runner/work/ranger-policy/ranger-policy/target"
+ls /home/runner/work/ranger-policy/ranger-policy/target
