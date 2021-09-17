22c58ad7613b7fea09e9b9c2349570c31281c0b7
surajbathina-atlan <suraj.bathina@atlan.com>
2021-09-17 14:35:47 +0530

    Adding a -Pall flag to build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Maven Building"
-mvn -pl '!plugin-kylin,!ranger-kylin-plugin-shim,!plugin-yarn,!ranger-yarn-plugin-shim,!plugin-solr,!ranger-solr-plugin-shim,!plugin-kafka,!ranger-kafka-plugin-shim,!plugin-presto,!ranger-presto-plugin-shim' -DskipJSTests -DskipTests=true -Drat.skip=true clean package
+mvn -pl '!plugin-kylin,!ranger-kylin-plugin-shim,!plugin-yarn,!ranger-yarn-plugin-shim,!plugin-solr,!ranger-solr-plugin-shim,!plugin-kafka,!ranger-kafka-plugin-shim,!plugin-presto,!ranger-presto-plugin-shim' -DskipJSTests -DskipTests=true -Drat.skip=true clean package -Pall
 
 echo "[DEBUG] listing distro/target"
 ls distro/target
