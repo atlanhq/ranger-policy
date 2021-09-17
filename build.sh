97cb1a92327098c12efd07e1b6f19e6df2a3800b
surajbathina-atlan <suraj.bathina@atlan.com>
2021-09-17 15:46:51 +0530

    Removing solr plugin

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Maven Building"
-mvn -pl '!plugin-kylin,!ranger-kylin-plugin-shim' -DskipJSTests -DskipTests=true -Drat.skip=true clean compile package install
+mvn -pl '!plugin-kylin,!ranger-kylin-plugin-shim,!plugin-solr,!ranger-solr-plugin-shim' -DskipJSTests -DskipTests=true -Drat.skip=true clean compile package install
 
 echo "[DEBUG] listing distro/target"
 ls distro/target
