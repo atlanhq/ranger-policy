8c9a98670b741fc8a90de2ea2bd7a42181ab5562
surajbathina-atlan <suraj.bathina@atlan.com>
2021-09-17 16:26:44 +0530

    Attempting build with ranger-admin profile

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Maven Building"
-mvn -pl '!plugin-kylin,!ranger-kylin-plugin-shim' -DskipJSTests -DskipTests=true -Drat.skip=true clean package
+mvn -DskipJSTests -DskipTests=true -Drat.skip=true clean package -Pranger-admin
 
 echo "[DEBUG] listing distro/target"
 ls distro/target
