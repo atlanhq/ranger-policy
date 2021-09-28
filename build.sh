50ab09684113ef24e0fe0ef4a1f84bcaa1c56521
surajbathina-atlan <suraj.bathina@atlan.com>
2021-09-28 20:15:13 +0530

    Removing solr plugin

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@
 # limitations under the License
 
 echo "Maven Building"
-mvn -pl '!plugin-kylin,!ranger-kylin-plugin-shim' -DskipJSTests -DskipTests=true -Drat.skip=true clean package -Pall
+mvn -pl '!plugin-kylin,!ranger-kylin-plugin-shim,!plugin-solr,!ranger-solr-plugin-shim' -DskipJSTests -DskipTests=true -Drat.skip=true clean package -Pall
 
 echo "[DEBUG] listing /home/runner/work/ranger-policy/ranger-policy/target"
 ls /home/runner/work/ranger-policy/ranger-policy/target
