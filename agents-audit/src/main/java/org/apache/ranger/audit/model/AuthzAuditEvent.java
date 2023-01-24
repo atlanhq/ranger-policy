d1f05bff5b39fae483352ba9c7c97660f9fb6463
Suman Das <59254445+sumandas0@users.noreply.github.com>
2023-01-24 23:29:55 +0530

    add: update the audit field to support guid in field

--- agents-audit/src/main/java/org/apache/ranger/audit/model/AuthzAuditEvent.java
+++ agents-audit/src/main/java/org/apache/ranger/audit/model/AuthzAuditEvent.java
@@ -148,7 +148,7 @@ public class AuthzAuditEvent extends AuditEventBase {
 			String resultReason, String aclEnforcer, String sessionId,
 			String clientType, String clientIP, String requestData, String clusterName, String zoneName) {
 		this(repositoryType, repositoryName, user, eventTime, accessType, resourcePath, resourceType, action, accessResult, agentId,
-				policyId, resultReason, aclEnforcer, sessionId, clientType, clientIP, requestData, clusterName, zoneName, null);
+				policyId, resultReason, aclEnforcer, sessionId, clientType, clientIP, requestData, clusterName, zoneName, null, null);
 
 	}
 
@@ -157,7 +157,18 @@ public class AuthzAuditEvent extends AuditEventBase {
 						   String resourcePath, String resourceType, String action,
 						   short accessResult, String agentId, long policyId,
 						   String resultReason, String aclEnforcer, String sessionId,
-						   String clientType, String clientIP, String requestData, String clusterName, String zoneName, Long policyVersion) {
+						   String clientType, String clientIP, String requestData, String clusterName, String zoneName, String entityGuid) {
+		this(repositoryType, repositoryName, user, eventTime, accessType, resourcePath, resourceType, action, accessResult, agentId,
+				policyId, resultReason, aclEnforcer, sessionId, clientType, clientIP, requestData, clusterName, zoneName, null, entityGuid);
+
+	}
+
+	public AuthzAuditEvent(int repositoryType, String repositoryName,
+						   String user, Date eventTime, String accessType,
+						   String resourcePath, String resourceType, String action,
+						   short accessResult, String agentId, long policyId,
+						   String resultReason, String aclEnforcer, String sessionId,
+						   String clientType, String clientIP, String requestData, String clusterName, String zoneName, Long policyVersion, String entityGuid) {
 		this.repositoryType = repositoryType;
 		this.repositoryName = repositoryName;
 		this.user = user;
@@ -178,6 +189,7 @@ public class AuthzAuditEvent extends AuditEventBase {
 		this.clusterName = clusterName;
 		this.zoneName = zoneName;
 		this.policyVersion = policyVersion;
+		this.entityGuid = entityGuid;
 	}
 
 	/**
